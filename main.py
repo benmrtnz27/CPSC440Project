54551a7c3dc542fdbf7ba6a37d39ad78bfbf14a8
benmrtnz27 <benmrtnz27@gmail.com>
2023-03-01 20:50:21 -0800

    Created 2 recommendation functions

--- main.py
+++ main.py
@@ -23,17 +23,44 @@ if len(sys.argv) > 1:
 else:
     name = what_artist
 
-# SEARCH FOR ARTISTS WITH q='artist:' AND MAKE SURE OUR RESULTS ARE LIMITED TO ARTISTS WITH type='artist'
-results = sp.search(q='artist:' + name, type='artist')
+def get_artist(name):
+    '''Function to get artists. This will be used as arguments for other functions.'''
+    results = sp.search(q="artist:" + name, type="artist")
+    items = results['artists']['items']
+    if len(items) > 0:
+        # print(items)  If you want to see the metadata to an artist
+        return items[0] 
+    else:
+        return None
+    
+def show_track_recommendations_for_artist(artist):
+    '''Returns songs based on an artist.'''
+    print(artist['name'] + ' Recommened Tracks:')
+    results = sp.recommendations(seed_artists=[artist['id']])
+    # print(results)
+    for track in results['tracks']:
+        if track['artists'][0]['name'] != artist['name']:
+            print(track['artists'][0]['name'] + ": " + track['name'])
 
-# ITEMS = EVERY ARTIST FOUND FROM THE SEARCH + THEIR METADATA
-items = results['artists']['items']
+def show_artists_recommendations_for_artist(artist):
+    '''Returns artists based on an artist.'''
+    print(artist['name'] + " Recommended Artists:")
+    results = sp.recommendations(seed_artists=[artist['id']])
+    artist_list = set()
+    for artist_rec in results['tracks']:
+        artist_list.add(artist_rec['artists'][0]['name']) 
+    artist_list.discard(artist['name'])
+    for artist_name in artist_list:
+        print(artist_name)
 
-# IF NO RESULT DO NOTHING. IF AT LEAST 1 RESULT DO THE FOLLOWING
-if len(items) > 0:
-    # items[0] LIMITS OUR RESULTS TO THE TOP ARTIST AKA THE ARTIST WHO MATCHES OUR INPUT THE CLOSEST
-    artist = items[0]
-    # artist['uri'] IS ESSENTIALLY A UNIQUE CODE TIED TO EVERY ARTIST ON SPOTIFY
-    # TO US IT'S IMPORTANT CAUSE ONCE WE HAVE IT WE CAN ACCESS THAT ARTIST A LOT FASTER BY NOT HAVING TO SERCH AGAIN
-    artist_id = artist['uri']
-    print("Name: " + artist['name'] + "\n" + "ID: " + artist_id)
+
+artist = get_artist(name)
+answer = input("What do you want to be recommened? \n1. Songs \n2. Artists \n")
+if answer == "1":
+    show_track_recommendations_for_artist(artist)
+    playlist_create_prompt = input("Would you like to create a playlist out of the recommended tracks? (1) Or just play the songs? (2)")
+    # TODO
+elif answer == "2": 
+    show_artists_recommendations_for_artist(artist)
+    artist_recommendations_get_songs_prompt = input("Would you like to create a playlist out of the recommended artists? (1) Or just play their top songs? (2)")
+    # TODO
